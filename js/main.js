807aef47e95a3e6eec03d0b81e027359cd7eb8b2
Vichna CHEAM <vichnana.cheam@gmail.com>
2024-03-18 11:52:05 +0100

    adjust file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+// THIS IS SEYHA'S CODE
+
+function displayRecipes(target, menuItems, footerFunction) {
+    menuItems.forEach((item, index) => {
+        const recipeDiv = document.createElement("div");
+        recipeDiv.classList.add("recipe-item");
+        recipeDiv.innerHTML = `
+            <div class="card">
+                <div class="card-content">
+                       ${generateRecipeCardContent(item)}
+                </div>
+                <div class="card-footer">
+                    ${footerFunction(index)}
+                </div>
+            </div>`;
+        target.appendChild(recipeDiv);
+    });
+}
+
+
+/**
+ * Generates the HTML content of a recipe's card
+ */
+function generateRecipeCardContent(recipe) {
+    let ingredientList = recipe.ingredients.map(ingredient => {
+        return `<li>${ingredient}</li>`;
+    }).join('');
+
+    return `
+        <h3 class="card-title">${recipe.name}</h3>
+        <img class="card-image" src="${recipe.image}" alt="Menu image">
+        <p class="card-description"><strong>Ingredients:</strong></p>
+        <ul class="card-description">${ingredientList}</ul>
+        <p class="card-description"><strong>Method:</strong></p>
+        <p class="card-description">${recipe.method}</p>
+    `;
+}
+
+// THIS IS VICHNA'S CODE
 //Function for narbar
 function myFunction() {
   var x = document.getElementById("myTopnav");
@@ -10,31 +49,6 @@ function myFunction() {
 // Display recipes function and other parts of your code remain the same
 const recipeList = document.querySelector('#recipe-list');
 const noRecipes = document.getElementById('no-recipes');
-function displayRecipes() {
-  recipeList.innerHTML = '';
-
-  recipes.forEach((recipe, index) => {
-      const recipeDiv = document.createElement('div');
-      recipeDiv.innerHTML = `
-          <div class="card">
-              <div class="card-content">
-                  <h3 class="card-title">${recipe.name}</h3>
-                  <img class="card-image" src="${recipe.image}">
-                  <p class="card-description"><strong>Ingredients:</strong></p>
-                  <p class="card-description">${recipe.ingredients.join('<br>')}</p>
-                  <p class="card-description"><strong>Method:</strong></p>
-                  <p class="card-description">${recipe.method}</p>
-                  <button class="edit-button" data-index="${index}">Edit</button>
-                  <button class="delete-button" data-index="${index}">Delete</button>
-              </div>
-          </div>`;
-          recipeDiv.classList.add('recipe');
-      recipeList.appendChild(recipeDiv);
-  });
-
-  noRecipes.style.display = recipes.length > 0 ? 'none' : 'flex';
-}
-
 const searchBox = document.getElementById('search-box');
 
 function search(query) {
